87ed2dba57e199ae008d4fa53889f46f0fee936d
Adrian Lacheta <adiczq@gmail.com>
2023-06-11 12:54:22 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+import { Profile } from './profile/Profile';
+import user from '../data/user';
+import { Statistics } from './statistics/Statistics';
+import data from '../data/data';
+import { FriendList } from './friendList/FriendList';
+import friends from '../data/friends';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
+import transactions from '../data/transactions';
+
+export const App = ({ children }) => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
-      React homework template
+      {children}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        likes={user.stats.likes}
+        views={user.stats.views}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
